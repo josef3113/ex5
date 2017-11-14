68d4f2ef734b74abf54439b6d0a270417b51d084
katanmichael <michaelkatan@gmail.com>
2017-11-14 14:03:38 +0200

    Testing Copy Constructor

--- main.cpp
+++ main.cpp
@@ -7,58 +7,73 @@ using namespace std;
 
 void main ()
 {
-	int x=7;
-	Rational num1,num2(3,4),num3;
-	num1.setdenominator(2).setnumerator(1);
-	cout<<"num1"<<endl;
-	num1.printnum();
-	cout<<"num2"<<endl;
-	num2.printnum();
+	Rational num1(1, 2);
+	Rational num2(1, 4);
+	Rational num3(num2);
 
-	cout<<"test whit int\n"<<"num2 is: "<<num2<<"x is:"<<x<<endl;
-	cout<<"num2+x=";
-	num2=num2+x;
-	cout<<num2;
-	cout<<"\nnum2 is: "<<num2<<"x is:"<<x<<endl;
-	cout<<"num2-x=";
-	num2=num2-x;
-	cout<<num2;
-	cout<<"\nnum2 is: "<<num2<<"x is:"<<x<<endl;
-	cout<<"num2*x=";
-	num2=num2*x;
-    cout<<num2;
-	cout<<"\nnum2 is: "<<num2<<"x is:"<<x<<endl;
-	cout<<"num2/x=";
-	num2=num2/x;
-	cout<<num2;
+	cout << num1 + num2 << " -- " << num1 << " + " << num2 << endl;
+	num2.setdenominator(num2.getdenominator() + 1);
 
-
-	cout<<"\ntest whit Rational\n"<<"num1 is: "<<num1<<"num2 is:"<<num2<<endl;
-	cout<<"num1+num2=";
-	num1=num1+num2;
-	//num1.printnum();
-	cout<<num1;
-	cout<<"\nnum1 is: "<<num1<<"num2 is:"<<num2<<endl;
-	cout<<"num1-num2=";
-	num1=num1-num2;
-	//num1.printnum();
-	cout<<num1;
-	cout<<"\nnum1 is: "<<num1<<"num2 is:"<<num2<<endl;
-	cout<<"num1*num2=";
-	num1=num1*num2;
-	//num1.printnum();
-	cout<<num1;
-	cout<<"\nnum1 is: "<<num1<<"num2 is:"<<num2<<endl;
-	cout<<"num1/num2=";
-	num1=num1/num2;
-	num1.printnum();
-	
-	cout<<"the opposite is"<<endl;
-	num1=!(num1);
-	//num1.printnum();
-	cout<<num1;
-	cout<<"insert num3"<<endl;
-	cin>>num3;
-	cout<<num3;
-    system ("pause");
+	cout << num1 + num2 <<  " -- " << num1 <<" + " << num2 << endl;
+	cout << num1 + num3 << " -- " << num1 << " + " << num3 << endl;
 }
+
+/*
+int x=7;
+Rational num1,num2(3,4),num3;
+num1.setdenominator(2).setnumerator(1);
+cout<<"num1"<<endl;
+num1.printnum();
+cout<<"num2"<<endl;
+num2.printnum();
+
+cout<<"test whit int\n"<<"num2 is: "<<num2<<"x is:"<<x<<endl;
+cout<<"num2+x=";
+num2=num2+x;
+cout<<num2;
+cout<<"\nnum2 is: "<<num2<<"x is:"<<x<<endl;
+cout<<"num2-x=";
+num2=num2-x;
+cout<<num2;
+cout<<"\nnum2 is: "<<num2<<"x is:"<<x<<endl;
+cout<<"num2*x=";
+num2=num2*x;
+cout<<num2;
+cout<<"\nnum2 is: "<<num2<<"x is:"<<x<<endl;
+cout<<"num2/x=";
+num2=num2/x;
+cout<<num2;
+
+
+cout<<"\ntest whit Rational\n"<<"num1 is: "<<num1<<"num2 is:"<<num2<<endl;
+cout<<"num1+num2=";
+num1=num1+num2;
+//num1.printnum();
+cout<<num1;
+cout<<"\nnum1 is: "<<num1<<"num2 is:"<<num2<<endl;
+cout<<"num1-num2=";
+num1=num1-num2;
+//num1.printnum();
+cout<<num1;
+cout<<"\nnum1 is: "<<num1<<"num2 is:"<<num2<<endl;
+cout<<"num1*num2=";
+num1=num1*num2;
+//num1.printnum();
+cout<<num1;
+cout<<"\nnum1 is: "<<num1<<"num2 is:"<<num2<<endl;
+cout<<"num1/num2=";
+num1=num1/num2;
+num1.printnum();
+
+cout<<"the opposite is"<<endl;
+num1=!(num1);
+//num1.printnum();
+cout<<num1;
+cout<<"insert num3"<<endl;
+cin>>num3;
+cout<<num3;
+system ("pause");
+
+
+
+*/
